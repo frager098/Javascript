a611e9e9719e6351febd86dd3c9882231ccee1d3
frager_098 <hafizmuhammadarham042@gmail.com>
2024-07-13 20:27:26 -0700

    Add Task 3

--- day1.js
+++ day1.js
@@ -8,3 +8,5 @@ console.log(myVariable);
 
 let myNewVariable ; myNewVariable = "I'm very exited for this challenge!" ;
 console.log(myNewVariable);
+//Task 3
+const t = 1 ; const f = 0 ; console.log(t,f)
