51373e2c9c813300ef6dcbab39167f40da1e5b2d
frager_098 <hafizmuhammadarham042@gmail.com>
2024-07-13 20:35:10 -0700

    Add Task 4

--- day1.js
+++ day1.js
@@ -10,3 +10,9 @@ let myNewVariable ; myNewVariable = "I'm very exited for this challenge!" ;
 console.log(myNewVariable);
 //Task 3
 const t = 1 ; const f = 0 ; console.log(t,f)
+//Task 4
+let firstVariable = 100 ;
+let secondVariable = "string";
+let thirdVariable = 0 ;
+let array = [1,2,3,4]
+console.log(typeof(firstVariable),typeof(secondVariable),typeof(thirdVariable),typeof(array))
