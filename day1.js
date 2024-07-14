db262a78117c2cfa04c1800a6a0c01c5e5965661
frager_098 <hafizmuhammadarham042@gmail.com>
2024-07-13 20:25:03 -0700

    Add Task 2

--- day1.js
+++ day1.js
@@ -1,7 +1,10 @@
                                 //Day 1
                                 //Activity 1
 //Task1
-var myVariable ; myVariable = "I'm very exited for this challenge!";
+var myVariable ; myVariable = 2;
 console.log(myVariable);
 
 //Task 2
+
+let myNewVariable ; myNewVariable = "I'm very exited for this challenge!" ;
+console.log(myNewVariable);
