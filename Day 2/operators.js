0b4ce3ffdd7fcd62bbe3a127539cf9e66c2aa51f
frager_098 <hafizmuhammadarham042@gmail.com>
2024-07-15 14:48:02 -0700

    Add Tasks of Arithmetic operations

---
+++ Day 2/operators.js
@@ -0,0 +1,26 @@
+                                //Day 2
+                        //Activity 1 Arithmetic Operations
+//Task 1
+//Write a program to add two numbers and log the result to the console
+// let a = +prompt("Enter 1st Number");
+// let b = +prompt("Enter 2nd Number") ;
+// let c = a + b ;
+// console.log(c);
+//Task 2 
+//Write a program to sub two numbers and log the result to the console
+// let a1 = +prompt("Enter 1st Number");
+// let b1 = +prompt("Enter 2nd Number") ;
+// let c1 = a1 - b1 ;
+// console.log(c1);
+//Task 3 
+//Write a program to multiply two numbers and log the result to the console
+// let a2 = +prompt("Enter 1st Number");
+// let b2 = +prompt("Enter 2nd Number") ;
+// let c2 = a2 * b2 ;
+// console.log(c2);
+//Task 4 
+//Write a program to divide two numbers and log the result to the console
+let a3 = +prompt("Enter 1st Number");
+let b3 = +prompt("Enter 2nd Number") ;
+let c3 = a3 / b3 ;
+console.log(c3);
