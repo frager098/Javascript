f1c58e354b2ece64efa86f319d17e3685c0a065e
frager_098 <hafizmuhammadarham042@gmail.com>
2024-07-26 16:04:35 -0700

    Add Activity 1

---
+++ Day 6/arrays.js
@@ -0,0 +1,7 @@
+//Activity 1 Array Creation and Access
+//Task 1 : Create and array of numbers from 1 to 5 and log the result to the console
+const arr = [1,2,3,4,5] ;
+console.log(arr);
+//Task 2 :Access the first and last element of the array and log the result to the console
+console.log(arr[0],arr[arr.length - 1]);
+//Activity 2 Array Methods(Basic)
