89007e114590411f3fd7ecc411599d71711a3367
frager_098 <hafizmuhammadarham042@gmail.com>
2024-07-26 16:10:39 -0700

    Add First 2 Tasks of Activity 2

--- Day 6/arrays.js
+++ Day 6/arrays.js
@@ -1,7 +1,13 @@
 //Activity 1 Array Creation and Access
 //Task 1 : Create and array of numbers from 1 to 5 and log the result to the console
 const arr = [1,2,3,4,5] ;
-console.log(arr);
+// console.log(arr);
 //Task 2 :Access the first and last element of the array and log the result to the console
-console.log(arr[0],arr[arr.length - 1]);
+// console.log(arr[0],arr[arr.length - 1]);
 //Activity 2 Array Methods(Basic)
+//Task 3 : Use push method to add a new number to the end of the array and log the updated array 
+// console.log(arr.push(6)); It returning the added element
+// console.log(arr);
+//Task 4 : Use pop method to remove the last element from the array and log the updated array 
+console.log(arr.pop()); // returning the deleted element
+console.log(arr)
