7cc75c187396e35025bd6ea7b1505679b4169c13
frager_098 <hafizmuhammadarham042@gmail.com>
2024-08-09 15:08:16 -0700

    Add Mouse Events

--- Day 10 Events/eventsHandlers.js
+++ Day 10 Events/eventsHandlers.js
@@ -10,3 +10,14 @@ function toggle(){
     const image = document.getElementById('img1');
     image.classList.toggle("toggling")
 }
+// Activity 2 : Mouse Events
+//Task  and 4 : Add a mouseover Event Listener to an element that changes it's background color and add a mouseout event listener that sets it's background color.
+function changeBackground() { 
+    // this.style.backgroundColor = "pink" // Not Working 
+    const elem = document.getElementById('link') ;
+    elem.style.backgroundColor = 'pink';
+    
+    elem.addEventListener('mouseout' , function (){
+        elem.style.backgroundColor = 'white';
+    })
+ };
