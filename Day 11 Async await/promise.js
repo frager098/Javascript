2c35f39952fd4511922e278549145d172bbfc3af
frager_098 <hafizmuhammadarham042@gmail.com>
2025-01-04 17:58:23 -0800

    Add 2nd Promise and some additional learnings

--- Day 11 Async await/promise.js
+++ Day 11 Async await/promise.js
@@ -1,13 +1,61 @@
-const promiseOne = new Promise( function (resolve , failure){
-    //Async Task
-    console.log("Async task is comepleted");
-    resolve();
-    failure();
-}
+// const promiseOne = new Promise( function (resolve , failure){
+//     //Async Task
+//     console.log("Async task is comepleted");
+//     resolve();
+//     failure(); //It will not execute as resolve callback will be executed
+// }
 
+// )
+// promiseOne.then( function(){
+//     console.log("Promise is Consumed"); //resolve
+// },function(){
+//     console.log("Promise is not consumed"); //failure
+// })
+
+const promiseTwo = new Promise(function (resolve , reject ) {
+    error = false;
+    setTimeout(function(){
+        if (!error){
+            console.log("Another Async Task Completed")
+            resolve({
+                name : "Arham" , id : "123"
+            })
+        }else{
+            reject("error")
+        }
+
+    } ,1000 )
+}
 )
-promiseOne.then( function(){
-    console.log("Promise is Consumed");
-},function(){
-    console.log("Promise is not consumed");
+const naming = promiseTwo.then((user)=>{
+    console.log(user)
+    console.log("resolved")
+    return user.name ;
+    
+},function(error){
+    console.log(error);
+
 })
+.then(
+    function(name){
+        console.log(name)
+    }
+)
+
+
+// const naming = promiseTwo.then((user)=>{
+//     console.log(user)
+//     console.log("resolved")
+//     return user.name ;
+    
+// },function(error){
+//     console.log(error);
+
+// })
+// const dataa = Object(naming) //Can't get like that 
+// setInterval( () => {
+
+//     console.log(dataa.PromiseResult)
+// }  ,1100
+
+// )
